676e4bf8c173b6ff1c3d3e86c4ea13d15596f831
paiizz <wanchanapon.t@ku.th>
2017-06-13 12:32:20 +0900

    initial Argon

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,22 +5,18 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	public ArgonScripts argonPerfab;
-	//	private Vector3 randomPosition;
 	public List<ArgonScripts> argons = new List<ArgonScripts> ();
+	// private number of argon molecular
+	private int numberArgon = 10 ;
 	// Use this for initialization
 	void Start () {
-		argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
-		argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
-		argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
-		argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
-		argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
-		foreach (ArgonScripts argon in argons)
-		{
-			argon.GetComponent<Rigidbody>().velocity = new Vector3(
-				Random.Range(-5.0f, 5.0f),
-				Random.Range(-5.0f, 5.0f),
-				Random.Range(-5.0f, 5.0f));
+		for(int i = 0 ; i < numberArgon ; i++){
+			argons.Add (Instantiate(argonPerfab,new Vector3(Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f),Random.Range(-9.0f, 9.0f)),Quaternion.identity));
 		}
+//		foreach (ArgonScripts argon in argons)
+//		{
+//			argon.GetComponent<Rigidbody>().velocity = new Vector3( Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f) );
+//		}
 	}
 
 	// Update is called once per frame
