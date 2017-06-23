0a6c21543d456cd89197b54ff85840a425350b6e
paiizz <wanchanapon.t@ku.th>
2017-06-23 12:50:45 +0900

    bug force

--- Assets/Scripts/ArgonScript.cs
+++ Assets/Scripts/ArgonScript.cs
@@ -16,52 +16,61 @@ public class ArgonScript : MonoBehaviour {
 	private Vector3 velocityVector;
 	private Vector3 momentumVector;
 	private Vector3 forceVector;
-	public GameController gameController;
+	private Vector3 positionVector;
+	private GameObject go;
+	private GameController gameController;
+	List<Vector3> listMomentum = new List<Vector3>();
+
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
-		gameController = GameController.getInstance ();
+		
+		GameObject go = GameObject.Find("GameController");
+		gameController = (GameController) go.GetComponent(typeof(GameController));
 
+		rb = GetComponent<Rigidbody>();
+//		Debug.Log (4*128*Mathf.Pow(342,12)+" eiei " + 4*128*Mathf.Pow(342,6));
 		alpha = Random.Range (-10.0f, 10.0f);
 		beta = Random.Range (-10.0f, 10.0f);
 		gamma = Random.Range (-10.0f, 10.0f);
 
 		calculateValue = Mathf.Pow (alpha, 2) + Mathf.Pow (beta, 2) + Mathf.Pow (gamma, 2);
-		Debug.Log ("calculateValue: " + calculateValue);
+//		Debug.Log ("calculateValue: " + calculateValue);
 
 		scalar = Mathf.Sqrt (calculateValue);
-		Debug.Log ("scaleVector: " + scalar);
+//		Debug.Log ("scaleVector: " + scalar);
 
 		unitVector = 1.0f / (scalar) * scalar;
-		Debug.Log ("unitVector: " + unitVector);
+//		Debug.Log ("unitVector: " + unitVector);
 
 		velocityVector = new Vector3(alpha * Mathf.Sqrt ((3 * R * T) / (massArgon*calculateValue)) , beta * Mathf.Sqrt ((3 * R * T) / (massArgon*calculateValue)), gamma * Mathf.Sqrt ((3 * R * T) / (massArgon*calculateValue)));
-		Debug.Log ("velocity x:" + velocityVector.x +" y:" + velocityVector.y + " z:"+velocityVector.z);
+//		Debug.Log ("velocity x:" + velocityVector.x +" y:" + velocityVector.y + " z:"+velocityVector.z);
 
 		momentumVector = massArgon * velocityVector;
-		Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
+//		Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
 
 //		rb.velocity = momentum ;
-		for(int i = 0 ; i < 1 ; i++){
-			Debug.Log ("x:" + gameController.gameObject.transform.GetChild(0).position.x+" y:" +  gameController.gameObject.transform.GetChild(0).position.y+" z:" +  gameController.gameObject.transform.GetChild(0).position.z);
-		}
-//		
+//		for(int i = 0 ; i < gameController.getNumberArgon() ; i++){
+//			if(this.transform.position.x != gameController.gameObject.transform.GetChild(i).position.x){
+//			Debug.Log ("x:" + gameController.gameObject.transform.GetChild(i).position.x+" y:" +  gameController.gameObject.transform.GetChild(i).position.y+" z:" +  gameController.gameObject.transform.GetChild(i).position.z);
+//			}
+//		}
 	}
 
-	public Vector3 getForce(ArgonScript obj){
-		float wellDepth = 342.0f; //constant well depth of argon
-		float diameter = 128.0f;  //constant diameter of argon
-		float scalePosition = Mathf.Sqrt ( Mathf.Pow((this.transform.position.x - obj.transform.position.x),2) + 
-										   Mathf.Pow((this.transform.position.y - obj.transform.position.y),2) + 
-										   Mathf.Pow((this.transform.position.z - obj.transform.position.z),2) );
+	public Vector3 calculationcForce(Vector3 obj){
+		float wellDepth = 128.0f; //constant well depth of argon
+		float diameter = 342.0f;  //constant diameter of argon
+		float scalePosition = Mathf.Sqrt ( Mathf.Pow((this.transform.position.x - obj.x),2) + 
+										   Mathf.Pow((this.transform.position.y - obj.y),2) + 
+										   Mathf.Pow((this.transform.position.z - obj.z),2) );
 //		Debug.Log ("scalePosition : " + scalePosition);
 		float A = 4*wellDepth*Mathf.Pow(diameter,12);
 		float B = 4*wellDepth*Mathf.Pow(diameter,6);
 		float energy = 12 * A * Mathf.Pow (scalePosition, -14) - 6 * B * Mathf.Pow (scalePosition, -8);
-		float forceX = energy*(this.transform.position.x - obj.transform.position.x);
-		float forceY = energy*(this.transform.position.y - obj.transform.position.y);
-		float forceZ = energy*(this.transform.position.z - obj.transform.position.z);
-
+		Debug.Log ("energy "+energy);
+		float forceX = energy*(this.transform.position.x - obj.x);
+		float forceY = energy*(this.transform.position.y - obj.y);
+		float forceZ = energy*(this.transform.position.z - obj.z);
+//		Debug.Log ("Force x:"+forceX+" y:"+forceY+" z:"+forceZ);
 		return new Vector3(forceX,forceY,forceZ);
 	}
 
@@ -80,64 +89,23 @@ public class ArgonScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-//		this.transform.GetChild (0);
-
+		
+		for(int i = 0 ; i < gameController.getNumberArgon() ; i++){
+			if(this.transform.position.x != gameController.transform.GetChild (i).position.x){
+			listMomentum.Add (momentumVector + 0.5f * Time.deltaTime * forceVector);
+//			momentumVector = momentumVector + 0.5f * Time.deltaTime * forceVector;
+			positionVector = new Vector3 (gameController.transform.GetChild (i).position.x, gameController.transform.GetChild (i).position.y, gameController.transform.GetChild (i).position.z) + (Time.deltaTime*momentumVector/massArgon);
+			Debug.Log ("positionVector x:"+positionVector.x + " y:"+positionVector.y+" z:"+positionVector.z);
+			forceVector = calculationcForce (positionVector);
+			listMomentum.Add (listMomentum[i] + 0.5f * Time.deltaTime * forceVector);
+			listMomentum.RemoveAt (i);
+			}
+		}
+		foreach(Vector3 momentum in listMomentum){
+			momentumVector += momentum ;
+		}
+//		rb.velocity = momentumVector;
+//		Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-//	void OnTriggerEnter(Collider other)
-//	{
-//		if (partner == null && other.gameObject.CompareTag("Argon"))
-//		{
-//			ArgonScript otherArgon = (ArgonScript)other.gameObject.GetComponent("Argon");
-//
-//			if (otherArgon.partner == null) // two free radicals meet and form covalent bond
-//			{
-//				partner = other.gameObject;
-//				otherArgon.partner = this.gameObject;
-//
-//				// chemical bond formation suddenly pulls slightly closer together
-//				float deltaX = partner.transform.position.x - this.transform.position.x;
-//				float deltaY = partner.transform.position.y - this.transform.position.y;
-//				float deltaZ = partner.transform.position.z - this.transform.position.z;
-//				this.transform.position = new Vector3(
-//					this.transform.position.x + 0.25f * deltaX,
-//					this.transform.position.y + 0.25f * deltaY,
-//					this.transform.position.z + 0.25f * deltaZ);
-//				partner.transform.position = new Vector3(
-//					partner.transform.position.x - 0.25f * deltaX,
-//					partner.transform.position.y - 0.25f * deltaY,
-//					partner.transform.position.z - 0.25f * deltaZ);
-
-				// create SpringJoint to implement covalent bond between these two atoms
-//				springJoint = this.gameObject.AddComponent<SpringJoint>();
-//				springJoint.connectedBody = other.gameObject.GetComponent<Rigidbody>();
-//				springJoint.anchor = new Vector3(0, 0, 0);
-//				springJoint.connectedAnchor = new Vector3(0, 0, 0);
-//				springJoint.spring = 10;
-//				springJoint.minDistance = 0.0f;
-//				springJoint.maxDistance = 0.0f;
-//				springJoint.tolerance = 0.025f;
-//				springJoint.breakForce = Mathf.Infinity;
-//				springJoint.breakTorque = Mathf.Infinity;
-//				springJoint.enableCollision = false;
-//				springJoint.enablePreprocessing = true;
-//			}
-//		}
-//		if (other.gameObject.CompareTag("Wall"))
-//		{
-//			audioSource.PlayOneShot(audioClipBallBounce, 0.25f);
-//		}
-//	}
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject gameObject;
 	public ArgonScript argonPerfab;
 	public List<ArgonScript> argons = new List<ArgonScript> ();
 	// private number of argon molecular
-	private int numberArgon = 1 ;
-	private static GameController instance; 
+	private int numberArgon = 3 ;
 
-	public static GameController getInstance(){
-		if (instance == null) instance = new GameController ();
-		return instance;
-	}
 	// Use this for initialization
 	void Start () {
 		for(int i = 0 ; i < numberArgon ; i++){
@@ -22,12 +16,12 @@ public class GameController : MonoBehaviour {
 		}
 
 		foreach (ArgonScript argon in argons) {
-			argon.transform.SetParent (gameObject.transform);
+			argon.transform.SetParent (this.transform);
 		}
 
-//		for(int i = 0 ; i < numberArgon ; i++){
-//			Debug.Log ("x:" + gameObject.transform.GetChild(i).position.x+" y:" +  gameObject.transform.GetChild(i).position.y+" z:" +  gameObject.transform.GetChild(i).position.z);
-//		}
+		for(int i = 0 ; i < numberArgon ; i++){
+			Debug.Log ("x:" + this.transform.GetChild(i).position.x+" y:" +  this.transform.GetChild(i).position.y+" z:" +  this.transform.GetChild(i).position.z);
+		}
 	}
 
 	// Update is called once per frame
