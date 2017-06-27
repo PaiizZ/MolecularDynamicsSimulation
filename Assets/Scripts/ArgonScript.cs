f2549d3bc198b82902826c86e4804c6163f45ace
paiizz <wanchanapon.t@ku.th>
2017-06-27 15:53:52 +0900

    change size wall

--- Assets/Scripts/ArgonScript.cs
+++ Assets/Scripts/ArgonScript.cs
@@ -51,22 +51,24 @@ public class ArgonScript : MonoBehaviour {
 //		Debug.Log ("velocity x:" + velocityVector.x +" y:" + velocityVector.y + " z:"+velocityVector.z);
 
 		momentumVector = massArgon * velocityVector;
-//		Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
+        //		Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
 
-//		rb.velocity = momentum ;
-//		for(int i = 0 ; i < gameController.getNumberArgon() ; i++){
-//			if(this.transform.position.x != gameController.gameObject.transform.GetChild(i).position.x){
-//			Debug.Log ("x:" + gameController.gameObject.transform.GetChild(i).position.x+" y:" +  gameController.gameObject.transform.GetChild(i).position.y+" z:" +  gameController.gameObject.transform.GetChild(i).position.z);
-//			}
-//		}
-	}
+		rb.velocity = momentumVector;
+        //		for(int i = 0 ; i < gameController.getNumberArgon() ; i++){
+        //			if(this.transform.position.x != gameController.gameObject.transform.GetChild(i).position.x){
+        //			Debug.Log ("x:" + gameController.gameObject.transform.GetChild(i).position.x+" y:" +  gameController.gameObject.transform.GetChild(i).position.y+" z:" +  gameController.gameObject.transform.GetChild(i).position.z);
+        //			}
+        //		}
+    }
 
-	public Vector3 calculationcForce(Vector3 obj){
+    public Vector3 calculationcForce(Vector3 obj){
 		float wellDepth = 1.65f * Mathf.Pow(10,-21); //constant well depth of argon
 		float diameter = 3.4f * Mathf.Pow(10, -10);  //constant diameter of argon
-        Debug.Log("wellDepth " + wellDepth);
-        Debug.Log("diameter " + diameter);
-        Debug.Log("scalePosition x :" + this.transform.position.x +"," +obj.x + " y :" + this.transform.position.y + "," + obj.y + " z :" + this.transform.position.z + "," + obj.z);
+ //       float wellDepth = 0.65f ; //constant well depth of argon
+ //       float diameter = 0.4f;  //constant diameter of argon
+ //       Debug.Log("wellDepth " + wellDepth);
+ //       Debug.Log("diameter " + diameter);
+        //Debug.Log("scalePosition x :" + this.transform.position.x +"," +obj.x + " y :" + this.transform.position.y + "," + obj.y + " z :" + this.transform.position.z + "," + obj.z);
         float scalePosition = Mathf.Sqrt ( Mathf.Pow((this.transform.position.x - obj.x),2) + 
 										   Mathf.Pow((this.transform.position.y - obj.y),2) + 
 										   Mathf.Pow((this.transform.position.z - obj.z),2) );
@@ -90,13 +92,14 @@ public class ArgonScript : MonoBehaviour {
 		for(int i = 0 ; i < gameController.getNumberArgon() ; i++){
 			if(this.transform.position.x != gameController.transform.GetChild (i).position.x){
      			temp = momentumVector + 0.5f * Time.deltaTime * forceVector;
-////			momentumVector = momentumVector + 0.5f * Time.deltaTime * forceVector;
+//			momentumVector = momentumVector + 0.5f * Time.deltaTime * forceVector;
 			    positionVector = new Vector3 (gameController.transform.GetChild (i).position.x, gameController.transform.GetChild (i).position.y, gameController.transform.GetChild (i).position.z) + (Time.deltaTime*momentumVector/massArgon);
 //			    Debug.Log ("positionVector x:"+positionVector.x + " y:"+positionVector.y+" z:"+positionVector.z);
 			    forceVector = calculationcForce (positionVector);
                 Debug.Log("Force x:" + forceVector.x + " y:" + forceVector.y + " z:" + forceVector.z);
                 listMomentum.Add (temp + 0.5f * Time.deltaTime * forceVector);
-                //Debug.Log("momentun list x:" + listMomentum[i].x + " y:" + listMomentum[i].y + " z:" + listMomentum[i].z);
+                Debug.Log("momentun temp x:" + temp.x + " y:" + temp.y + " z" +
+                    ":" + temp.z);
             }
 		}
         foreach (Vector3 momentum in listMomentum)
@@ -104,7 +107,7 @@ public class ArgonScript : MonoBehaviour {
     		momentumVector += momentum ;
 		}
         listMomentum.Clear();
-        //rb.velocity = momentumVector;
+//        rb.velocity = momentumVector;
         Debug.Log ("momentum x:" + momentumVector.x +" y:" + momentumVector.y + " z:"+momentumVector.z);
 	}
 
