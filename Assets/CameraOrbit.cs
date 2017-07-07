784f72a728c91bf6c080e80fc130e5ed281c2663
paiizz <wanchanapon.t@ku.th>
2017-07-07 18:24:11 +0900

    create script camera

---
+++ Assets/CameraOrbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour {
+    protected Transform _XFpom_Camera;
+    protected Transform _XFpom_Parent;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Last Update is called once per frame, after Update() on every game object in the senced.
+    void Update () {
+		
+	}
+}
